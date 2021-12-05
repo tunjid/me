f591751fe684f7e009c3fdda09f4f5302ad86890
Adetunji Dahunsi <tjdah100@gmail.com>
2021-12-05 16:32:58 -0500

    Basic ArchiveMutator implementation

--- app/src/main/java/com/tunjid/me/ui/archive/ArchiveMutator.kt
+++ app/src/main/java/com/tunjid/me/ui/archive/ArchiveMutator.kt
@@ -16,6 +16,21 @@
 
 package com.tunjid.me.ui.archive
 
+import com.tunjid.me.data.Api
+import com.tunjid.mutator.Mutation
+import com.tunjid.mutator.Mutator
+import com.tunjid.mutator.coroutines.stateFlowMutator
+import com.tunjid.mutator.coroutines.toMutationStream
+import com.tunjid.tiler.Tile
+import com.tunjid.tiler.flattenWith
+import com.tunjid.tiler.tiledList
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.map
+
 data class State(
     val archives: List<Archive> = listOf()
 )
@@ -24,15 +39,51 @@ data class ArchiveQuery(
     val year: Int?,
     val month: Int?
 )
+
 sealed class Action {
     data class Load(
         val kind: ArchiveKind,
         val before: ArchiveQuery,
         val offset: Int,
         val limit: Int = 20
-    )
+    ) : Action()
 }
 
-fun archiveMutator() {
+fun archiveMutator(
+    scope: CoroutineScope,
+    api: Api
+): Mutator<Action, StateFlow<State>> = stateFlowMutator(
+    scope = scope,
+    initialState = State(),
+    started = SharingStarted.WhileSubscribed(2000),
+    transform = { actions ->
+        actions.toMutationStream {
+            when (val action = type()) {
+                is Action.Load -> action.flow
+                    .toArchives(api = api)
+                    .map { archives ->
+                        Mutation { copy(archives = archives) }
+                    }
+            }
+        }
+    }
+)
 
-}
+private fun Flow<Action.Load>.toArchives(api: Api) =
+    map { Tile.Request.On<Action.Load, List<Archive>>(it) }
+        .flattenWith(tiledList(
+            flattener = Tile.Flattener.PivotSorted(
+                comparator = compareBy(Action.Load::offset),
+            ),
+            fetcher = { load ->
+                flowOf(
+                    api.fetchArchives(
+                        kind = load.kind,
+                        options = mapOf(
+                            "offset" to load.offset.toString()
+                        )
+                    )
+                )
+            }
+        ))
+        .map { it.flatten() }
