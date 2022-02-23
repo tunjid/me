706859fe48bf447ab16369f7bc296629b41ffc9e
Adetunji Dahunsi <tjdah100@gmail.com>
2022-02-23 16:36:26 -0500

    Clean up

--- common/scaffold/src/commonMain/kotlin/com/tunjid/me/scaffold/nav/RouteParser.kt
+++ common/scaffold/src/commonMain/kotlin/com/tunjid/me/scaffold/nav/RouteParser.kt
@@ -16,41 +16,9 @@
 
 package com.tunjid.me.scaffold.nav
 
-data class RouteParams(
-    val path: String,
-    val params: Map<String, String>
-)
-
 interface RouteParser<T : AppRoute> {
     val pattern: String
     fun route(matchResult: MatchResult): T
-//    fun mutator(routeScope: CoroutineScope, route: R): T
-
-    fun String.parseRoute(): RouteParams {
-        val pathIndex = indexOf('?')
-        return try {
-            when {
-                pathIndex < 0 || pathIndex == lastIndex -> RouteParams(
-                    path = this,
-                    params = mapOf()
-                )
-                else -> RouteParams(
-                    path = substring(0, pathIndex),
-                    params = substring(pathIndex + 1, lastIndex)
-                        .split("&")
-                        .associate {
-                            val keyValue = it.split("=")
-                            keyValue[0] to keyValue[1]
-                        }
-                )
-            }
-        } catch (e: Exception) {
-            RouteParams(
-                path = this,
-                params = mapOf()
-            )
-        }
-    }
 }
 
 fun <T : AppRoute> routeParser(
