2cb9babf9bc6702b8cede4ccaf0e067ecddbd52a
dahunsi <tjdah100@gmail.com>
2023-09-25 18:11:58 -0700

    Add stub for backPreviewModifier on desktop

--- common/scaffold/build/generated/ksp/desktop/desktopMain/kotlin/com/tunjid/me/scaffold/globalui/scaffold/AppScaffold.desktop.kt
+++
@@ -1,8 +0,0 @@
-package com.tunjid.me.scaffold.globalui.scaffold
-
-import androidx.compose.ui.Modifier
-
-/**
- * Modifier that offers a way to preview content behind the primary content
- */
-internal actual fun Modifier.backPreviewModifier(): Modifier = Modifier
---
+++ common/scaffold/src/desktopMain/kotlin/com/tunjid/me/scaffold/globalui/scaffold/AppScaffold.desktop.kt
@@ -0,0 +1,5 @@
+package com.tunjid.me.scaffold.globalui.scaffold
+
+import androidx.compose.ui.Modifier
+
+internal actual fun Modifier.backPreviewModifier(): Modifier = Modifier
