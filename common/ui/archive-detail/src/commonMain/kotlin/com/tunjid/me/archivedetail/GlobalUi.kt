f0e5ddb67629809fe9d0607f2ecc013a2998855e
dahunsi <tjdah100@gmail.com>
2023-12-02 14:38:16 -0800

    Allow navigating to gallery from the detail screen

--- common/ui/archive-detail/src/commonMain/kotlin/com/tunjid/me/archivedetail/GlobalUi.kt
+++ common/ui/archive-detail/src/commonMain/kotlin/com/tunjid/me/archivedetail/GlobalUi.kt
@@ -18,24 +18,46 @@ package com.tunjid.me.archivedetail
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Email
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.toArgb
+import com.tunjid.me.core.ui.icons.Preview
 import com.tunjid.me.scaffold.globalui.InsetFlags
 import com.tunjid.me.scaffold.globalui.NavVisibility
 import com.tunjid.me.scaffold.globalui.ScreenUiState
 import com.tunjid.me.scaffold.globalui.UiState
 import com.tunjid.me.scaffold.globalui.currentUiState
 import com.tunjid.me.scaffold.globalui.rememberFunction
+import com.tunjid.me.scaffold.globalui.slices.ToolbarItem
 import com.tunjid.treenav.push
 import com.tunjid.treenav.strings.routeString
 
 @Composable
-fun GlobalUi(state: State, actions: (Action) ->  Unit) {
+fun GlobalUi(state: State, actions: (Action) -> Unit) {
     ScreenUiState(
         UiState(
             toolbarShows = true,
             toolbarTitle = state.archive?.title ?: "Detail",
+            toolbarItems = listOf(
+                ToolbarItem(
+                    id = "gallery",
+                    text = "Gallery",
+                    imageVector = Icons.Default.Email
+                )
+            ),
+            toolbarMenuClickListener = rememberFunction {
+                when (it.id) {
+                    "gallery" -> if (state.archive != null) actions(Action.Navigate {
+                        navState.push(
+                            routeString(
+                                path = "archives/${state.kind.type}/${state.archive.id.value}/files",
+                                queryParams = emptyMap()
+                            ).toRoute
+                        )
+                    })
+                }
+            },
             navVisibility = NavVisibility.Visible,
             // Prevents UI from jittering as load starts
             fabShows = if (state.hasFetchedAuthStatus) state.canEdit else currentUiState.fabShows,
