66016653cf6fa1df572c430581cd9fda7f16678f
Adetunji Dahunsi <tjdah100@gmail.com>
2024-03-09 13:00:36 -0500

    Update DeferredAnimation to match newly introduced DeferredTargetAnimation in upstream Compose

--- common/scaffold/src/commonMain/kotlin/com/tunjid/me/scaffold/adaptive/SharedElements.kt
+++ common/scaffold/src/commonMain/kotlin/com/tunjid/me/scaffold/adaptive/SharedElements.kt
@@ -10,23 +10,27 @@ import androidx.compose.animation.core.VisibilityThreshold
 import androidx.compose.animation.core.spring
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.Stable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.movableContentOf
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.produceState
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.layout.intermediateLayout
 import androidx.compose.ui.unit.Constraints
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.round
-import com.tunjid.scaffold.adaptive.Adaptive
 import com.tunjid.scaffold.adaptive.LocalAdaptiveContentScope
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 
 internal interface SharedElementScope {
@@ -49,36 +53,16 @@ internal class SharedElementData<T>(
     onRemoved: () -> Unit
 ) {
     private var inCount by mutableIntStateOf(0)
-    var canAnimate by mutableStateOf(true)
 
     val offsetAnimation = DeferredAnimation(
         vectorConverter = IntOffset.VectorConverter,
-        animationSpec = spring(
-            stiffness = Spring.StiffnessMediumLow,
-            dampingRatio = 0.9f,
-            visibilityThreshold = IntOffset.VisibilityThreshold
-        )
     )
     val sizeAnimation = DeferredAnimation(
         vectorConverter = IntSize.VectorConverter,
-        animationSpec = spring(
-            stiffness = Spring.StiffnessMediumLow,
-            visibilityThreshold = IntSize.VisibilityThreshold
-        )
     )
 
     val moveableSharedElement: @Composable (Any?, Modifier) -> Unit =
         movableContentOf { state, modifier ->
-            val scope = LocalAdaptiveContentScope.current
-            SideEffect {
-                when (scope?.containerState?.container) {
-                    Adaptive.Container.Primary -> canAnimate = scope.transition.isRunning
-                    Adaptive.Container.Secondary -> canAnimate = scope.transition.isRunning
-                    Adaptive.Container.TransientPrimary -> canAnimate = false
-                    null -> Unit
-                }
-            }
-
             @Suppress("UNCHECKED_CAST")
             sharedElement(
                 // The shared element composable will be created by the first screen and reused by
@@ -105,71 +89,136 @@ internal class SharedElementData<T>(
  */
 internal fun Modifier.sharedElement(
     sharedElementData: SharedElementData<*>,
-): Modifier = this then intermediateLayout { measurable, _ ->
-    val (width, height) = sharedElementData.sizeAnimation.updateTarget(
-        coroutineScope = this,
-        targetValue = lookaheadSize,
+): Modifier = this then composed {
+    val sizeAnimComplete = sharedElementData.isComplete(
+        SharedElementData<*>::sizeAnimation
     )
-    val animatedConstraints = Constraints.fixed(width, height)
-    val placeable = measurable.measure(animatedConstraints)
+    val offsetAnimComplete = sharedElementData.isComplete(
+        SharedElementData<*>::offsetAnimation
+    )
+    intermediateLayout { measurable, _ ->
+        val (width, height) = sharedElementData.sizeAnimation.updateTarget(
+            coroutineScope = this,
+            target = lookaheadSize,
+            animationSpec = sizeSpec,
+        )
+        val animatedConstraints = Constraints.fixed(width, height)
+        val placeable = measurable.measure(animatedConstraints)
 
-    layout(placeable.width, placeable.height) layout@{
-        val currentCoordinates = coordinates ?: return@layout placeable.place(
-            x = 0,
-            y = 0
-        )
-        val targetOffset = lookaheadScopeCoordinates.localLookaheadPositionOf(
-            currentCoordinates
-        )
-        val animatedOffset = sharedElementData.offsetAnimation.updateTarget(
-            coroutineScope = this@intermediateLayout,
-            targetValue = targetOffset.round(),
-        )
+        layout(placeable.width, placeable.height) layout@{
+            val currentCoordinates = coordinates ?: return@layout placeable.place(
+                x = 0,
+                y = 0
+            )
+            val targetOffset = lookaheadScopeCoordinates.localLookaheadPositionOf(
+                currentCoordinates
+            )
+            val animatedOffset = sharedElementData.offsetAnimation.updateTarget(
+                coroutineScope = this@intermediateLayout,
+                target = targetOffset.round(),
+                animationSpec = offsetSpec,
+            )
 
-        if (!sharedElementData.canAnimate) return@layout placeable.place(
-            x = 0,
-            y = 0
-        )
+            if (sizeAnimComplete && offsetAnimComplete) return@layout placeable.place(
+                x = 0,
+                y = 0
+            )
 
-        val currentOffset = lookaheadScopeCoordinates.localPositionOf(
-            sourceCoordinates = currentCoordinates,
-            relativeToSource = Offset.Zero
-        ).round()
+            val currentOffset = lookaheadScopeCoordinates.localPositionOf(
+                sourceCoordinates = currentCoordinates,
+                relativeToSource = Offset.Zero
+            ).round()
 
-        val (x, y) = animatedOffset - currentOffset
-        placeable.place(
-            x = x,
-            y = y
-        )
+            val (x, y) = animatedOffset - currentOffset
+            placeable.place(
+                x = x,
+                y = y
+            )
+        }
     }
 }
 
 @Stable
 internal class DeferredAnimation<T, V : AnimationVector>(
     private val vectorConverter: TwoWayConverter<T, V>,
-    private val animationSpec: FiniteAnimationSpec<T>
 ) {
-    val value: T? get() = animatable?.value ?: target
+    /**
+     * Returns the target value from the most recent [updateTarget] call.
+     */
+    val pendingTarget: T?
+        get() = _pendingTarget
 
-    private var target: T? by mutableStateOf(null)
+    private var _pendingTarget: T? by mutableStateOf(null)
+    private val target: T?
+        get() = animatable?.targetValue
     private var animatable: Animatable<T, V>? = null
 
+    /**
+     * [updateTarget] sets up an animation, or updates an already running animation, based on the
+     * [target] in the given [coroutineScope]. [pendingTarget] will be updated to track the last
+     * seen [target].
+     *
+     * [updateTarget] will return the current value of the animation after launching the animation
+     * in the given [coroutineScope].
+     *
+     * @return current value of the animation
+     */
     fun updateTarget(
+        target: T,
         coroutineScope: CoroutineScope,
-        targetValue: T,
-    ): T = with(coroutineScope) {
-        target = targetValue
-        if (target != null && target != animatable?.targetValue) {
-            when (val currentAnimatable = animatable) {
-                null -> animatable = Animatable(targetValue, vectorConverter)
-                else -> launch {
-                    currentAnimatable.animateTo(
-                        targetValue = targetValue,
-                        animationSpec = animationSpec
-                    )
-                }
+        animationSpec: FiniteAnimationSpec<T> = spring()
+    ): T {
+        _pendingTarget = target
+        val anim = animatable ?: Animatable(target, vectorConverter).also { animatable = it }
+        coroutineScope.launch {
+            if (anim.targetValue != _pendingTarget) {
+                anim.animateTo(target, animationSpec)
             }
         }
-        return animatable?.value ?: targetValue
+        return anim.value
     }
+
+    /**
+     * [isIdle] returns true when the animation has finished running and reached its
+     * [pendingTarget], or when the animation has not been set up (i.e. [updateTarget] has never
+     * been called).
+     */
+    val isIdle: Boolean
+        get() = _pendingTarget == target && animatable?.isRunning != true
 }
+
+@Composable
+private fun SharedElementData<*>.isComplete(
+    animationMapper: (SharedElementData<*>) -> DeferredAnimation<*, *>
+): Boolean {
+    val animation = remember { animationMapper(this) }
+    val adaptiveContentScope = LocalAdaptiveContentScope.current
+    val scopeKey = adaptiveContentScope?.key
+    var seenKeyCount by remember { mutableIntStateOf(0) }
+
+    DisposableEffect(scopeKey) {
+        seenKeyCount++
+        onDispose { }
+    }
+    val animationCompleteAtLeastOnce by produceState(
+        initialValue = false,
+        key1 = scopeKey
+    ) {
+        value = false
+        value = snapshotFlow { animation.isIdle }
+            .filter(true::equals)
+            .first()
+    }
+    return seenKeyCount < 1 || animationCompleteAtLeastOnce
+}
+
+private val sizeSpec = spring(
+    stiffness = Spring.StiffnessMediumLow,
+    visibilityThreshold = IntSize.VisibilityThreshold
+)
+
+private val offsetSpec = spring(
+    stiffness = Spring.StiffnessMediumLow,
+    dampingRatio = 0.9f,
+    visibilityThreshold = IntOffset.VisibilityThreshold
+)
